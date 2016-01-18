df6252e5fa949380feba9a5355587164b626f65d
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2016-01-18 15:52:49 -0800

    [structure/feat] Add button to switch views and generate random hash (sorry this hould be two commits)

--- client/app/app_routes.js
+++ client/app/app_routes.js
@@ -1,11 +1,16 @@
 angular.module('clientRoutes', [])
 
-.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', function($stateProvider, $urlRouterProvider, $httpProvider) {
+.config(['$stateProvider', function($stateProvider) {
 
   $stateProvider
 
+    .state('home', {
+    url: '/',
+    templateUrl: './app/components/home/homeView.html'
+  })
     .state('test', {
     url: '/test',
-    templateUrl: './app/components/feat_sample/feat.html'
-  });
+    templateUrl: './app/components/feat_sample/featView.html'
+  })
+    ;
 }]);
--- client/app/components/feat_sample/feat.js
+++
@@ -1,9 +0,0 @@
-angular.module('feat', [])
-
-.controller('featController', function($scope, $state) {
-
-  $scope.clicker = function() {
-    console.log('im executing');
-    $state.go('test');
-  };
-});
