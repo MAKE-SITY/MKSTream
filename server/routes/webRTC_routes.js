8be791ed64d42c93cd0ae6a2e091748c8d08e60d
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-18 18:59:23 -0800

    [feature] created peer routes in webRTC_routes.js

--- server/routes/webRTC_routes.js
+++ server/routes/webRTC_routes.js
@@ -1,3 +1,21 @@
 module.exports = function(app){
+	//temporary. for testing:
+	var tempArray = [];
+	app.post('/users', function(req, res){
+		tempArray.push(req.body.userId);
+		
+		res.status(200);
+		res.send(tempArray);
+		// console.log(req.body.userId);
+		// TODO: store caller userID, somehow
+			// tie to random link generated
+	});
 
+	app.get('/users', function(req, res){
+		res.status(200);
+		res.send(tempArray);
+	});
+
+	// When someone accesses one of the created links,
+	// make a request here for callee to recieve the caller userID
 };
