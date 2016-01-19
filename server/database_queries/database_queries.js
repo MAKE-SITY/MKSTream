52e029842ae3a18d4581338cc19f7f23d1aaeaad
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2016-01-19 14:01:42 -0800

    [feat] Implement add user to database with kcvan in database_queries.js

--- server/database_queries/database_queries.js
+++ server/database_queries/database_queries.js
@@ -1,25 +1,25 @@
-var User = require('./../database/models/user_schema');
+var User = require('./../../database/models/user_schema.js');
+var db = require('./../../database/config.js');
 
 var exportObj = {};
 
 exportObj.addUser = function(linkHash, senderID, receiverID) {
   new User({
-    linkHash: linkHash
-    senderID: senderID
+    linkHash: linkHash,
+    senderID: senderID,
     receiverID: receiverID
-  }).save(function(err) {
+  }).save(function(err, addedUser) {
     if(err) {
-      console.log('error trying to save user to DB', err);
+      console.log('error trying to save user to DB:', err);
     }
+    console.log(addedUser, 'addedUser');
   });
 };
 
-var addReceiver = function() {
-
-}
+var addReceiver = function(senderID, receiverID) {
+  User.update({senderID: senderID}, {});
+};
 
 var deleteUser = function() {
 
-}
-
-module.exports = exportObj;
+};
