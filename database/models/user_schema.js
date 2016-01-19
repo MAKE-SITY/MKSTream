956e377b74c0dcf8504587cf77b3516cb04ad198
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2016-01-19 15:17:55 -0800

    [refactor] Change schema attribute name with kcvan in user_schema.js

--- database/models/user_schema.js
+++ database/models/user_schema.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
   linkHash: String,
   senderID: String,
-  receiverID: Array
+  receiverIDArray: Array
 });
 
 module.exports = mongoose.model('User', userSchema);
