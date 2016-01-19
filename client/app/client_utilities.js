ad285f2198394964267f1ce9a405ec3226cf451a
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-19 12:08:01 -0800

    [refactor] refactored linkGeneration function from homeController to linkGeneration factory in client_utilities.js

--- client/app/client_utilities.js
+++ client/app/client_utilities.js
@@ -95,4 +95,47 @@ angular.module('utils', [])
     getFiles: getFiles
   };
 
+}])
+.factory('linkGeneration', [function(){
+  var guid = function() {
+    var s4 = function() {
+      return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    };
+    return s4() + s4() + s4() + s4() + s4() + s4() + s4() + s4();
+  };
+
+  return {
+    guid: guid
+  };
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
