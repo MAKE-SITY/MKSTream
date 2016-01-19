89919fda644ae444de118a643e61ce4a52779fa7
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-18 19:04:22 -0800

    [style] removed console.logs in webRTC_routes.js

--- server/routes/webRTC_routes.js
+++ server/routes/webRTC_routes.js
@@ -1,12 +1,12 @@
 module.exports = function(app){
-	//temporary. for testing:
+	//temporary.storage for testing:
 	var tempArray = [];
+
 	app.post('/users', function(req, res){
 		tempArray.push(req.body.userId);
-		
 		res.status(200);
 		res.send(tempArray);
-		// console.log(req.body.userId);
+
 		// TODO: store caller userID, somehow
 			// tie to random link generated
 	});
