97587f5276a8ea257625d6ab80336407fc38b2fb
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-19 15:03:26 -0800

    [refactor] The sender peer is created here & generates a link to share in homeController.js

--- client/app/components/home/homeController.js
+++ client/app/components/home/homeController.js
@@ -2,16 +2,54 @@ angular.module('home', [
   'utils'
 ])
 
-.controller('homeController', ['$scope', '$state', '$stateParams', '$location', '$rootScope', 'fileUpload', 'linkGeneration', function($scope, $state, $stateParams, $location, $rootScope, fileUpload, linkGeneration) {
-  $scope.myItems = [];
+.controller('homeController', ['$scope', '$http', '$state', '$stateParams', '$location', '$rootScope', 'fileUpload', 'linkGeneration', 'webRTC', function($scope, $http, $state, $stateParams, $location, $rootScope, fileUpload, linkGeneration, webRTC) {
+  $rootScope.myItems = [];
   $scope.file = 'sample.txt';
 
-  $scope.generateLink = function() {
+  var generateLink = function() {
     $scope.hash = linkGeneration.guid();
     $stateParams.test = $scope.hash;
     $location.path('/' + $scope.hash);
     console.log($stateParams);
   };
+
+  document.getElementById('filesId').addEventListener('change', function() {
+
+    var files = this.files;
+    for (var i = 0; i < files.length; i++) {
+      $rootScope.myItems.push(files[i]);
+    }
+
+    console.log('myItems', $rootScope.myItems);
+
+
+    if (!$rootScope.peer) {
+      $rootScope.peer = webRTC.createPeer();
+      $rootScope.peer.on('open', function(id) {
+        // TODO: create special link to send with post in data
+        $http({
+          method: 'POST',
+          url: '/api/webrtc/users',
+          data: {
+            userId: id,
+            hash: $scope.hash
+          }
+        });
+      });
+
+      $rootScope.peer.on('connection', function(conn) {
+        // TODO: add file inside call to send
+        $rootScope.conn = conn;
+        console.log('does this ever happen', conn);
+        $rootScope.conn.on('data', function(data) {
+          console.log('data response from bitches', data);
+        });
+      });
+      generateLink();
+    }
+  });
+
+
 
   $scope.testParams = $stateParams;
 
