d5a37114cedbc1bb71d0059c9324881c3cadfee4
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2016-01-21 11:20:04 -0800

    [fix] Fix falsely passing test in server_spec.js

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,13 +1,13 @@
 var request = require('request');
-var server = require('./../server/server.js');
+// var server = require('./../server/server.js');
 var base_url = 'http://localhost:5000/';
 
-describe("Hello World Server", function() {
-  describe("GET /", function() {
-    it("returns status code 200", function() {
-      request.get(base_url, function(error, response, body) {
-        expect(response.statusCode).toBe(200);
-      });
+describe("Home page", function() {
+
+  it("should respond with status code 200", function(done) {
+    request(base_url, function(error, response, body) {
+      expect(response.statusCode).toBe(200);
+      done();
     });
   });
 });
