72a10b5f40cbcbd176ed2fb74aa901d359321247
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-22 11:01:44 -0800

    [refactor] peerjs server in server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-var peerPort = process.env.PEERPORT || 9000;
+// var peerPort = process.env.PEERPORT || 9000;
 var app = express();
 
 require('./config/middleware.js')(app, express);
 
-app.listen(port);
+var srv = app.listen(port);
 
 console.log('Now listening on port: ' + port);
 
@@ -15,9 +15,5 @@ var ExpressPeerServer = require('peer').ExpressPeerServer;
 var options = {
     debug: true
 };
-// create a http server instance to listen to request
-var server = require('http').createServer(app);
-// peerjs is the path that the peerjs server will be connected to.
-app.use('/peerjs', ExpressPeerServer(server, options));
-// Now listen to your ip and port.
-server.listen(peerPort, "localhost");
+
+app.use('/peerjs', ExpressPeerServer(srv, options));
