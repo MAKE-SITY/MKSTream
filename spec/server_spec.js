e8bfe8db377a8b445689713ea355e726b860fe1b
Simon Ding <simonding87@Simons-MacBook-Pro.local>
2016-01-21 19:35:44 -0800

    [test] Add deleteLink unit test to server_spec.js

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -5,19 +5,26 @@ var server = require('./../server/server.js');
 var dbQueries = require('./../server/database_queries/database_queries.js');
 var User = require('./../database/models/user_schema.js');
 
+var testNumber = 1;
+
+beforeEach(function() {
+  console.log('======Starting test #', testNumber);
+})
+
+afterEach(function() {
+  console.log('=======Finished with test #', testNumber);
+  testNumber++;
+})
 
 describe("Server", function() {
   it("should respond with status code 200", function(done) {
-    console.log('starting test 1');
     request(base_url, function(error, response, body) {
       expect(response.statusCode).toBe(200);
-      console.log('done with test 1');
       done();
     });
   });
 
   it("addLink should be able to add an item to the database", function(done) {
-    console.log('starting test 2');
     dbQueries.addLink('Test Hash MKS', 'Test Sender MKS').then(function() {
       User.findOne({
         linkHash: 'Test Hash MKS'
@@ -27,14 +34,12 @@ describe("Server", function() {
         }
       }).then(function(result) {
         expect(result.senderID).toBe('Test Sender MKS');
-        console.log('done with test 2');
         done();
       });
     });
   });
 
   it("addReceiverToSender should be able to add a receiverID to a sender", function(done) {
-    console.log('starting test 3');
     dbQueries.addReceiverToSender('Test Hash MKS', 'Test Receiver MKS').then(function() {
       User.findOne({
         linkHash: 'Test Hash MKS'
@@ -43,36 +48,32 @@ describe("Server", function() {
           return err;
         }
       }).then(function(result) {
-        console.log('Receiver ID Array:', result);
+        console.log('result:', result);
         expect(result.receiverIDArray[result.receiverIDArray.length - 1]).toBe('Test Receiver MKS');
-        console.log('done with test 3');
         done();
       });
     });
   });
 
   it("getSenderId be able to get user object from a link hash", function(done) {
-    console.log('starting test 4');
     dbQueries.getSenderId('Test Hash MKS').then(
       function(result) {
-        console.log('result', result)
         expect(result.senderID).toBe('Test Sender MKS');
-        console.log('done with test 4');
         done();
       })
   });
 
-  // xit("should be able to delete an item from the database", function(done) {
-  //   dbQueries.deleteLink('senderID1');
-  //   User.findOne({
-  //     senderID: 'senderID1'
-  //   }, function(err, user) {
-  //     if (err) {
-  //       return err;
-  //     }
-  //   }).then(function(result) {
-  //     expect(result.senderID).toBe(null);
-  //     done();
-  //   });
-  // });
+  it("should be able to delete an item from the database", function(done) {
+    dbQueries.deleteLink('senderID1');
+    User.findOne({
+      senderID: 'senderID1'
+    }, function(err, user) {
+      if (err) {
+        return err;
+      }
+    }).then(function(result) {
+      expect(result).toBe(null);
+      done();
+    });
+  });
 });
