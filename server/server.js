3167091da141cb46a8231f5b6ea5a82fdcb94cf2
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-21 19:46:46 -0800

    [feat] added expressPeerServer in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
+var peerPort = process.env.PEERPORT || 9000;
 var app = express();
 
 require('./config/middleware.js')(app, express);
@@ -7,3 +8,61 @@ require('./config/middleware.js')(app, express);
 app.listen(port);
 
 console.log('Now listening on port: ' + port);
+
+
+
+// create express peer server
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+var options = {
+    debug: true
+};
+// create a http server instance to listen to request
+var server = require('http').createServer(app);
+// peerjs is the path that the peerjs server will be connected to.
+app.use('/peerjs', ExpressPeerServer(server, options));
+// Now listen to your ip and port.
+server.listen(peerPort, "localhost");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // initialize express
+// var express = require('express');
+// var app1 = express();
+// // create express peer server
+// var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+// var options = {
+//     debug: true
+// };
+
+// // create a http server instance to listen to request
+// var server = require('http').createServer(app1);
+
+// // peerjs is the path that the peerjs server will be connected to.
+// app1.use('/peerjs', ExpressPeerServer(server, options));
+// // Now listen to your ip and port.
+// server.listen(9000, "localhost");
