1d09ba1fde58d050f54422e5166ab7bcd13a0d5e
kcvan <kcongvan@gmail.com>
2016-01-22 15:38:04 -0800

    [refactor] Refactor fileReaderFactory with SimonDing87 to fileReaderFactory.js

---
+++ client/app/components/client_utilities/factories/fileReaderFactory.js
@@ -0,0 +1,52 @@
+angular.module('utils.fileReader', [])
+
+.factory('fileReader', ['$q', '$log', function($q, $log) {
+  var fileReaderObj = {};
+
+  var onLoad = function(reader, deferred, scope) {
+    return function() {
+      scope.$apply(function() {
+        deferred.resolve(reader.result);
+      });
+    };
+  };
+
+  var onError = function(reader, deferred, scope) {
+    return function() {
+      scope.$apply(function() {
+        deferred.reject(reader.result);
+      });
+    };
+  };
+
+  var onProgress = function(reader, scope) {
+    return function(event) {
+      scope.$broadcast('fileProgress', {
+        total: event.total,
+        loaded: event.loaded
+      });
+    };
+  };
+
+  var getReader = function(deferred, scope) {
+    var reader = new FileReader();
+
+    reader.onload = onLoad(reader, deferred, scope);
+    reader.onerror = onError(reader, deferred, scope);
+    reader.onprogress = onProgress(reader, scope);
+
+    return reader;
+  };
+
+  fileReaderObj.readAsArrayBuffer = function(file, scope) {
+    var deferred = $q.defer();
+
+    var reader = getReader(deferred, scope);
+    reader.readAsArrayBuffer(file);
+
+    return deferred.promise;
+  };
+
+  return fileReaderObj;
+
+}]);
