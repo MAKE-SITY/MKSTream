0a176cfe337d68f57b7e7a52ec8d1ddb8db41b57
kcvan <kcongvan@gmail.com>
2016-01-23 11:31:55 -0800

    [feat] Add route to handle peer object destruction when window closed to webRTC_routes.js

--- server/routes/webRTC_routes.js
+++ server/routes/webRTC_routes.js
@@ -1,37 +1,43 @@
 var dbHelpers = require('../database_queries/database_queries.js');
 
-module.exports = function(app){
-	app.post('/users', function(req, res){
-		var packet = req.body;
-		if(packet.userId){
-			console.log('SENDER post event');
-			dbHelpers.addLink(packet.hash, packet.userId)
-			.then(function(result){
-				res.status(201);
-				res.send('link added');
-			});
-		} else {
-			console.log('RECEIVER post response event');
-			dbHelpers.addReceiverToSender(packet.hash, packet.recipientId)
-			.then(function(result) {
-				console.log('adding receiverToSender');
-				dbHelpers.getSenderId(packet.hash)
-				.then(function(result) {
-					res.status(200);	
-					res.send(result);
-				});
-			});
-		}
+module.exports = function(app) {
+  app.post('/users', function(req, res) {
+    var packet = req.body;
+    if (packet.userId) {
+      console.log('SENDER post event');
+      dbHelpers.addLink(packet.hash, packet.userId)
+        .then(function(result) {
+          res.status(201);
+          res.send('link added');
+        });
+    } else {
+      console.log('RECEIVER post response event');
+      dbHelpers.addReceiverToSender(packet.hash, packet.recipientId)
+        .then(function(result) {
+          console.log('adding receiverToSender');
+          dbHelpers.getSenderId(packet.hash)
+            .then(function(result) {
+              res.status(201);
+              res.send(result);
+            });
+        });
+    }
 
-		// TODO: store caller userID, somehow
-			// tie to random link generated
-	});
+    app.post('/delete', function(req, res) {
+      dbHelpers.deleteLink(req.body.userId);
+      res.status(201);
+      res.send("User deleted");
+    })
 
-	app.get('/users', function(req, res){
-		res.status(200);
-		res.send(      );
-	});
+    // TODO: store caller userId, somehow
+    // tie to random link generated
+  });
 
-	// When someone accesses one of the created links,
-	// make a request here for callee to recieve the caller userID
+  app.get('/users', function(req, res) {
+    res.status(200);
+    res.send();
+  });
+
+  // When someone accesses one of the created links,
+  // make a request here for callee to recieve the caller userId
 };
