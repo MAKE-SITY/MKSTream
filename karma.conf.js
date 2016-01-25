f741452baaf74895c214dfd82a7aacf9e830b7b9
kcvan <kcongvan@gmail.com>
2016-01-25 12:19:34 -0800

    [test] Add file path dependency to karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -15,23 +15,22 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-    'bower_components/angular/angular.js',
-    'bower_components/angular-mocks/angular-mocks.js',
-    'bower_components/angular-resource/angular-resource.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/angular-resource/angular-resource.js',
       'client/app/**/*.js',
-      'spec/factory_tests/**/*.js'
+      'spec/factory_tests/**/*.js',
+      'spec/controller_tests/**/*.js'
     ],
 
 
     // list of files to exclude
-    exclude: [
-    ],
+    exclude: [],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
+    preprocessors: {},
 
 
     // test results reporter to use
