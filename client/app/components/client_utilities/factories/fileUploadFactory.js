0b4a21eeb3fd35736bff6cbaa5cbf08ce34a2a8d
Simon Ding <SimonDing87@gmail.com>
2016-01-27 19:43:31 -0800

    [feat] Add file size converter helper function in fileUploadFactory.js

--- client/app/components/client_utilities/factories/fileUploadFactory.js
+++ client/app/components/client_utilities/factories/fileUploadFactory.js
@@ -15,6 +15,16 @@ angular.module('utils.fileUpload', ['utils.fileReader'])
     return kit;
   };
 
+  fileUploadObj.convert = function(num) {
+    if (num > 1000000000) {
+        return (num / 1000000000).toFixed(2) + ' GB'
+    } else if (num > 1000000) {
+        return (num / 1000000).toFixed(2) + ' MB'
+    } else  {
+        return (num / 1000).toFixed(2) + ' KB'
+    }
+  }
+
   return fileUploadObj;
 
 }]);
