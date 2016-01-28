7e36488be11cbc0ae129fdf20a2f597e73fb25a9
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-27 19:47:17 -0800

    [style] changed console.log messages in background.js

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -3,15 +3,15 @@ console.log('im the background page');
 chrome.runtime.onConnect.addListener(function(port) {
   console.assert(port.name === "knockknock");
   port.onMessage.addListener(function(msg) {
-  	console.log('fuck 1');
+  	console.log('hello 1');
     if (msg.joke === "Knock knock") {
-    	console.log('fuck 2');
+    	console.log('hello 2');
       port.postMessage({question: "Who's there?"});
     } else if (msg.answer === "Madame") {
-      console.log('fuck 3');
+      console.log('hello 3');
       port.postMessage({question: "Madame who?"});
     } else if (msg.answer === "Madame... Bovary") {
-    	console.log('fuck 4');
+    	console.log('hello 4');
       port.postMessage({question: "I don't get it."});
     }
   });
