c8930dee9fe1fc80f88e2f448501ea9d42288ce2
Tyler Ferrier <ferrier.tyler@gmail.com>
2016-01-30 17:44:16 -0800

    [refactor] added download and upload routes in app_routes.js

--- client/app/app_routes.js
+++ client/app/app_routes.js
@@ -11,9 +11,21 @@ angular.module('clientRoutes', [])
       controller: 'homeController',
       templateUrl: './app/components/home/homeView.html'
     })
-    .state('room', {
-      url: '/room/:roomHash',
-      controller: 'connectingController',
-      templateUrl: './app/components/connecting/featView.html'
+     .state('room', {
+      url: '/room/:roomHash', 
+      views: {
+        'connecting':{  
+          controller: 'connectingController'
+          // templateUrl: './app/components/connecting/featView.html'
+        },
+        'upload':{  
+          controller: 'uploadController',
+          templateUrl: './app/components/connecting/uploadView.html'
+        },
+        'download':{
+          controller: 'downloadController',
+          templateUrl: './app/components/connecting/downloadView.html'
+        }
+      }
     });
 }]);
