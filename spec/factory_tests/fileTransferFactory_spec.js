dfcbac035ec66c1e24794067f5f99c97fcaf4729
Simon Ding <SimonDing87@gmail.com>
2016-02-01 16:44:10 -0800

    [style] Rename some things in fileTransferFactory_spec.js

--- spec/factory_tests/fileTransferFactory_spec.js
+++ spec/factory_tests/fileTransferFactory_spec.js
@@ -1,14 +1,14 @@
-describe('fileTransferObj Factory', function() {
+describe('fileTransfer Factory', function() {
   var factory;
   beforeEach(function() {
-    module('utils.fileTransfer'); //angular.module name
+    module('utils'); //angular.module name
 
     inject(function($injector) {
       factory = $injector.get('fileTransfer'); //.factory name
     });
   });
 
-  describe('fileTransferObj', function() {
+  describe('fileTransfer', function() {
     it("Should return an object with 5 keys", function() {
       var myObj = Object.keys(factory).length;
       expect(myObj).toEqual(5);
