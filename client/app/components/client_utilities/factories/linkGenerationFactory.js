3f0b72832546c7c1868a9148e669f486d5878507
Simon Ding <SimonDing87@gmail.com>
2016-02-01 17:06:31 -0800

    [refactor] Rename factory return objects in /factories

--- client/app/components/client_utilities/factories/linkGenerationFactory.js
+++ client/app/components/client_utilities/factories/linkGenerationFactory.js
@@ -1,21 +1,21 @@
 angular.module('utils.linkGeneration', [])
 
 .factory('linkGeneration', [function() {
-  var linkGenerationObj = {};
+  var linkGeneration = {};
 
   var s4 = function() {
     //this is a random hash generator
     return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
   };
 
-  linkGenerationObj.guid = function() {
+  linkGeneration.guid = function() {
     return adjAdjAnimal(); // global function from adjective-adjective-animal bower package, returns promise
   };
 
-  linkGenerationObj.fuid = function() {
+  linkGeneration.fuid = function() {
     return s4() + s4() + s4() + s4();
   };
 
-  return linkGenerationObj;
+  return linkGeneration;
 
 }]);
