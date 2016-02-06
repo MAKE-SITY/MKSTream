53b0e837ecf977d48eb2ff447b623c8bf3d9769f
Simon Ding <SimonDing87@gmail.com>
2016-02-05 19:33:20 -0800

    [refactor] Add comments and fix syntax lint in linkGenerationFactory.js

--- client/app/components/client_utilities/factories/linkGenerationFactory.js
+++ client/app/components/client_utilities/factories/linkGenerationFactory.js
@@ -9,44 +9,46 @@ angular.module('utils.linkGeneration', [])
   };
 
   linkGeneration.adjAdjAnimal = function() {
-    return adjAdjAnimal(); // global function from adjective-adjective-animal bower package, returns promise
+    // global function from adjective-adjective-animal bower package, returns promise
+    return adjAdjAnimal();
   };
 
   linkGeneration.generateHash = function() {
+    // 16 character hash
     return s4() + s4() + s4() + s4();
   };
 
   linkGeneration.copyToClipboard = function(elem) {
-      // create hidden text element, if it doesn't already exist
-      var targetId = "_hiddenCopyText_";
-      var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-      var origSelectionStart, origSelectionEnd;
-      if (isInput) {
-        // can just use the original source element for the selection and copy
-        target = elem;
-        origSelectionStart = elem.selectionStart;
-        origSelectionEnd = elem.selectionEnd;
-      } else {
-        // must use a temporary form element for the selection and copy
-        target = document.getElementById(targetId);
-        if (!target) {
-          var target = document.createElement("textarea");
-          target.style.position = "absolute";
-          target.style.left = "-9999px";
-          target.style.top = "0";
-          target.id = targetId;
-          document.body.appendChild(target);
-        }
-        target.textContent = elem.textContent;
+    // create hidden text element, if it doesn't already exist
+    var targetId = "_hiddenCopyText_";
+    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
+    var origSelectionStart, origSelectionEnd, target;
+    if (isInput) {
+      // can just use the original source element for the selection and copy
+      target = elem;
+      origSelectionStart = elem.selectionStart;
+      origSelectionEnd = elem.selectionEnd;
+    } else {
+      // must use a temporary form element for the selection and copy
+      target = document.getElementById(targetId);
+      if (!target) {
+        target = document.createElement("textarea");
+        target.style.position = "absolute";
+        target.style.left = "-9999px";
+        target.style.top = "0";
+        target.id = targetId;
+        document.body.appendChild(target);
       }
-      // select the content
-      var currentFocus = document.activeElement;
-      target.focus();
-      target.setSelectionRange(0, target.value.length);
-
-      // copy the selection
-      return document.execCommand("copy");
+      target.textContent = elem.textContent;
     }
+    // select the content
+    var currentFocus = document.activeElement;
+    target.focus();
+    target.setSelectionRange(0, target.value.length);
+
+    // copy the selection
+    return document.execCommand("copy");
+  }
 
   return linkGeneration;
 
