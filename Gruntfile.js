56ee35aba86d9e78b0d8b65aad788c9af55eadea
Simon Ding <SimonDing87@gmail.com>
2016-02-08 19:18:51 -0800

    [chore] Update grunt task to include all dependencies in Grunfile.js, allmincode.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,9 +18,13 @@ module.exports = function(grunt) {
             'bower_components/adjective-adjective-animal/dist/adjective-adjective-animal.min.js',
             'bower_components/angular/angular.min.js',
             'bower_components/angular-ui-router/release/angular-ui-router.min.js',
+            'bower_components/angular-animate/angular-animate.min.js',
+            'bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js',
             'client/lib/nochunkbufferfixpeer.min.js',
             'bower_components/localforage/dist/localforage.min.js',
             'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/angular-ui-notification/dist/angular-ui-notification.min.js',
+            'bower_components/bootstrap/dist/js/bootstrap.min.js',
             'client/app/**/*.js'
           ]
         }
